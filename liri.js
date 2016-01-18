641875c0304ae0003063e9a629e175582a00196c
Rolando Cosme <rolandocosme@Rolandos-MBP.home>
2016-01-17 19:26:15 -0500

    completed function to get last 20 tweets but limited to 2 for now until I write more tweets

--- liri.js
+++ liri.js
@@ -1,23 +1,30 @@
-var keys = require("./keys.js");
-
-var params = process.argv.slice(3);
+debugger;
+var Twitter = require('twitter');
+var Keys = require("./keys.js");
 
 switch(process.argv[2]) {
   case "my-tweets":
-    result = tweets(params);
+    getTweets();
     break;
+  };
 
-  case "spotify-this-song":
-    result = subtract.doIt(params);
-    break;
-
-  case "move-this":
-    result = subtract.doIt(params);
-    break;
-
-  case "do-what-is-says":
-    result = subtract.doIt(params);
-    break;
-
-}
-console.log(result);
+function getTweets(){
+  var client = new Twitter({
+    consumer_key: 'KqkB0kmAhHDQq7dnRX2rHtSla',
+    consumer_secret: 'iOi3bKl35Gttm0LIfGkWbxovZ8KezZ5u1Kmjk9gvD4q9Ou9U3l',
+    access_token_key: '171743975-ZhAn6RrPE9rqcxkjc9wmlsoUltySJfinonr3al7n',
+    access_token_secret: 'eBqS3q5b1z7sP1gOUXHvdvXm183xE0Cw8mErvm1JbCT6K',
+  });
+   
+  var params = {screen_name: 'RolandoCosme'};
+  client.get('statuses/user_timeline', params, function(error, tweets, response){
+    if (!error) {
+      for (var i = 0; i < 2; i++) {
+        console.log(tweets[i].text);
+        };
+    }
+    else {
+      console.log(error);
+    };
+  });
+};
